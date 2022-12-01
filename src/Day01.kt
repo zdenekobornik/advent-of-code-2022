59f7959c7d1b69b69a5ca61761527224ac14bff8
Zdeněk Oborník <zdenek@obornik.cz>
2022-12-01 10:20:19 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        return input.split("\n\n").maxOf { it.split("\n").sumOf(String::toInt) }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        return input.split("\n\n")
+            .map { it.split("\n").sumOf(String::toInt) }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputRaw("Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readInputRaw("Day01")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -9,6 +9,11 @@ fun readInput(name: String) = File("src", "$name.txt")
     .readLines()
 
 /**
+ * Reads lines from the given input txt file.
+ */
+fun readInputRaw(name: String) = File("src", "$name.txt").readText()
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
